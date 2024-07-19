71fc1d26e37e240e64119a268c83f8ce28485f50
jesus-imanol <jesusimanolcastillo@gmail.com>
2024-07-19 13:08:27 -0600

    fix the login, prevention errors

--- src/assets/components/Molecules/Register/Form.jsx
+++ src/assets/components/Molecules/Register/Form.jsx
@@ -1,47 +1,53 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { setUser, getUser } from '../../../../../User';
+import toast, { Toaster } from 'react-hot-toast';
 
 function Form() {
+  const [loading, setLoading] = useState(false);
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
   const navigate = useNavigate();
 
   const handleLogin = (e) => {
     e.preventDefault();
+    setLoading(true);
     fetch(`${import.meta.env.VITE_URL}/users/login`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
+        'Access-Control-Allow-Origin': '*',
       },
       body: JSON.stringify({
         email: emailRef.current.value,
         password: passwordRef.current.value,
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          localStorage.setItem('token', response.headers.get('Authorization'));
+          return response.json();
+        } else {
+          toast.error('Oh, hubo un problema intente más tarde');
+          throw new Error('Error en la autenticación');
+        }
       })
-    })
-    .then(response => {
-      if (response.ok) {
-        localStorage.setItem('token',response.headers.get('Authorization'))
-        return response.json();
-      }
-      throw new Error('Error en la autenticación');
-    })
-    .then(data => {
-      const rolId = data.user.role_id_fk;
-      console.log(rolId);
-      const email = data.user.email;
-      const user_id = data.user.user_id;
-      const name = data.user.name;
-      setUser({rolId,email,user_id,name});
-      if(getUser().rolId===1)
-        navigate('/homeAdmin')
-      else
-      navigate('/')
-    })
-    .catch(error => {
-      console.log(error);
-    });
+      .then((data) => {
+        const { role_id_fk: rolId, email, user_id, name } = data.user;
+        setUser({ rolId, email, user_id, name });
+        if (getUser().rolId === 1) {
+          navigate('/homeAdmin');
+        } else {
+          navigate('/');
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        toast.error('Uppsss, algo malo ocurrió intente más tarde');
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
 
   return (
@@ -50,7 +56,10 @@ function Form() {
       <input type="text" ref={emailRef} />
       <h6>Password</h6>
       <input type="password" ref={passwordRef} />
-      <button onClick={handleLogin}>Iniciar sesión</button>
+      <button onClick={handleLogin} disabled={loading}>
+        {loading ? 'Cargando...' : 'Iniciar sesión'}
+      </button>
+      <Toaster />
     </>
   );
 }
